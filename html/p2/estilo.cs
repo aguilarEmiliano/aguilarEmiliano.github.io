0377241e5568582469931c9cd6fc23bf29521082
aguilarEmiliano <130393723+aguilarEmiliano@users.noreply.github.com>
2023-04-11 11:37:16 -0600

    Add files via upload

---
+++ html/p2/estilo.cs
@@ -0,0 +1,27 @@
+ p{
+            background-color:black;
+            margin: 25px 10px 5px 15px;
+        }
+        #pEspecial{
+            background-color:darkblue;
+            color:white;
+            font-family: 'Delicious Handrawn', cursive;
+font-family: 'Noto Sans Lepcha', sans-serif;
+        }
+        #Cuadro{
+            border: 5px solid black;
+        }
+        th,td{
+            border: 1px dotted gray
+        }
+        .alterno{
+            background-color: gray;
+        }
+        h1:hover,h2:hover{
+            color: green;
+            background-color: lightskyblue;
+
+        }
+        td:hover{
+            background-color:lightgreen 
+        }
